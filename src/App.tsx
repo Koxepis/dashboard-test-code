f0aaa6c2505fa47448e41f1497fc29fd773bd81b
Shar51 <andiasar11@gmail.com>
2022-01-06 23:58:27 +0800

    Initialize tailwindcss, vechaiui, and heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ColorScheme,
+  colors,
+  extendTheme,
+  VechaiProvider,
+  Button,
+} from "@vechaiui/react";
 
 function App() {
+  const bee: ColorScheme = {
+    id: "bee",
+    type: "light",
+    colors: {
+      bg: {
+        base: colors.warmGray["100"],
+        fill: colors.warmGray["100"],
+      },
+      text: {
+        foreground: colors.warmGray["900"],
+        muted: colors.warmGray["700"],
+      },
+      primary: colors.amber,
+      neutral: colors.warmGray,
+    },
+  };
+
+  const theme = extendTheme({
+    cursor: "pointer",
+    colorSchemes: {
+      bee,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <VechaiProvider theme={theme} colorScheme="bee">
+        <div>
+          <p>Test</p>
+        </div>
+      </VechaiProvider>
+    </>
   );
 }
 
