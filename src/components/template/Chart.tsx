48c2a4954851feabf91a1a6fbf2fcd18e37f6ba7
Shar51 <andiasar11@gmail.com>
2022-01-08 16:24:36 +0800

    implementing chart.js

--- src/components/template/Chart.tsx
+++ src/components/template/Chart.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BarChart from "../elements/BarChart";
+import LineChart from "../elements/LineChart";
 <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3"></dl>;
 
 const Chart = () => {
@@ -7,10 +8,10 @@ const Chart = () => {
     <div>
       <div className="relative mt-3">
         <div className="relative max-w-full mx-auto sm:mx-0">
-          <div className="max-w-4xl mx-auto">
-            <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+          <div className="max-w-4xl">
+            <dl className="rounded-lg bg-white shadow-lg">
               <BarChart />
-              <BarChart />
+              <LineChart />
             </dl>
           </div>
         </div>
