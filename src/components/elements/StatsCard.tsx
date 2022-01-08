e07e369f4840ccc3b9f7dd15da725358832dc85f
Shar51 <andiasar11@gmail.com>
2022-01-08 14:02:19 +0800

    remove background and horizontal margin

--- src/components/elements/StatsCard.tsx
+++ src/components/elements/StatsCard.tsx
@@ -5,13 +5,13 @@ interface Props {
 
 const StatsCard = (props: Props) => {
   return (
-    <div className="mt-10 pb-12 bg-white sm:pb-16">
+    <div className="mt-10 pb-12 sm:pb-16">
       <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">
         Current Stats
       </h2>
       <div className="relative mt-3">
         <div className="relative max-w-full mx-auto sm:mx-0">
-          <div className="max-w-4xl mx-auto">{props.children}</div>
+          <div className="max-w-full">{props.children}</div>
         </div>
       </div>
     </div>
