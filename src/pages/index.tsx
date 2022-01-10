642639677ba3733ec1da1353281fe5060711810e
Shar51 <andiasar11@gmail.com>
2022-01-10 08:44:25 +0800

    redirect landing page into admin dashboard
    empty commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/admin");
+  }, 1200);
+
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center">
       <p>this is a landing page</p>
     </div>
   );
