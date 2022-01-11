628c65674a2d39fca4c14c08dca2c9bbae1d0f40
Shar51 <andiasar11@gmail.com>
2022-01-11 13:16:25 +0800

    show multiple marker on map

--- src/components/elements/Map.tsx
+++ src/components/elements/Map.tsx
@@ -1,20 +1,43 @@
-import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import mockData from "./../../shared/helpers/MOCK_DATA.json";
 
 const MapComponent = () => {
+  console.log(mockData[0].address.city);
+
   return (
-    <div className="min-w-full pb-12 flex items-center justify-center mx-auto">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+    <div className="pb-12 flex flex-col items-center justify-center space-y-12">
+      <div className="min-w-full mx-auto">
+        <MapContainer
+          center={[1.292839, 103.859512]}
+          zoom={12}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution="Dashboard Test Code"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {mockData
+            .filter((location) => location.address.city === "Singapore")
+            .map((item) => (
+              <Marker
+                key={item.id}
+                position={
+                  [
+                    item.gps.latitude as number,
+                    item.gps.longitude as any,
+                  ] as any
+                }
+              >
+                <Popup>
+                  <p>Name: {item.name}</p>
+                  <p>Address: {item.address.street}</p>
+                  <p>Status: {item.status}</p>
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+      </div>
     </div>
   );
 };
